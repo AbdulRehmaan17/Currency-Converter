12787ffe3f67ac472694a6edcedf91b3a92bf2a8
Abdul Rehman <abdulrehman396387@gmail.com>
2025-09-24 02:09:23 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+const apiCurrency = "https://api.exchangerate.host/symbols";
+const apiConvert = "https://api.exchangerate.host/convert";
+
+let input1 = document.getElementById("amount1");
+let input2 = document.getElementById("amount2");
+let dropDown1 = document.getElementById("dropDown1");
+let dropDown2 = document.getElementById("dropDown2");
+let convertButton = document.getElementById("convertButton");
