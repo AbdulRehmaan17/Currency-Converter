f9c5f7b946f54e2586ed8a6d0a5d0ffb15451fb5
Abdul Rehman <abdulrehman396387@gmail.com>
2025-09-24 16:48:02 +0500

    Initial Commit

--- app.js
+++ app.js
@@ -1,10 +1,57 @@
 
+class currencyConverter{
+    constructor(){
+        this.apiCurrency = "https://api.frankfurter.dev/v1/currencies";
+        this.apiConvert = "https://api.frankfurter.dev/v1/latest";
 
-const apiCurrency = "https://api.exchangerate.host/symbols";
-const apiConvert = "https://api.exchangerate.host/convert";
+        this.input1 = document.getElementById("amount1");
+        this.input2 = document.getElementById("amount2");
+        this.dropDown1 = document.getElementById("dropDown1");
+        this.dropDown2 = document.getElementById("dropDown2");
+        this.convertButton = document.getElementById("convertButton");
+        this.convertButton.addEventListener("click", () => this.convertCurrency());
 
-let input1 = document.getElementById("amount1");
-let input2 = document.getElementById("amount2");
-let dropDown1 = document.getElementById("dropDown1");
-let dropDown2 = document.getElementById("dropDown2");
-let convertButton = document.getElementById("convertButton");
+        this.getCurrency();
+    }
+    async getCurrency(){
+        try{
+            let response = await fetch(this.apiCurrency);
+            let data = await response.json();
+
+            for(let currency in data){
+                let option1 = document.createElement("option");
+                option1.value = currency;
+                option1.text = currency;
+                this.dropDown1.appendChild(option1);
+                let option2 = document.createElement("option");
+                option2.value = currency;
+                option2.text = currency;
+                this.dropDown2.appendChild(option2);
+            }
+        }
+         catch(error){
+            alert("Error fetching currency data: " + error);
+        }
+
+    }
+    async convertCurrency(){
+        let amount1 = this.input1.value;
+        let from = this.dropDown1.value;
+        let to = this.dropDown2.value;
+
+        if(from === to){
+            this.input2.value = amount1;
+            return;
+        }
+        try{
+            let url = this.apiConvert + "?amount=" + amount1 + "&from=" + from + "&to=" + to;
+            let response = await fetch(url);
+            let data = await response.json();
+            this.input2.value = data.rates[to];
+        }
+        catch(error){
+            alert("Error converting currency: " + error);
+        }
+    }
+}
+new currencyConverter();
